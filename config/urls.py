a1aadb79f13a919fefae9b8a71c16d2c3a2198dc
abdulloh-id <outergamer11@gmail.com>
2024-07-21 10:31:08 +0500

    Added user signup feature

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('account/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')), # order is important here
+    path('accounts/', include('accounts.urls')),
 ]
